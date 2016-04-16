ed05cc738d0d49a287dd47f047cc438e94d4c43e
Jacob Caban-Tomski <jacob.caban-tomski@workiva.com>
2016-04-16 17:58:02 -0600

    Made camera follow player.

--- public/js/state/game.ts
+++ public/js/state/game.ts
@@ -9,6 +9,7 @@ class DUArcade2016Game extends Phaser.State {
     }
 
     create() {
+        this.game.world.setBounds(0, 0, 1920, 1920);
         this.game.physics.startSystem(Phaser.Physics.P2JS);
         this.game.physics.p2.setBoundsToWorld(false, false, false, false);
         this.level = new Level(this.game, new LevelData(this.cache.getJSON("test-level-0")));
