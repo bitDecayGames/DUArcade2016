d9a8cad5a6bad3f2ebdec7a69f5599a21fa0db59
Jacob Caban-Tomski <jacob.caban-tomski@workiva.com>
2016-04-16 16:20:19 -0600

    Added an endpoint to save a json file for a level and return the raw file in a POST request.

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,27 @@
 //<reference path="./definitions/node.d.ts"/>
 //<reference path="./definitions/express.d.ts"/>
 var express = require("express");
+var fs = require('fs');
 var app = express();
 var port = 3000;
 
 app.use(express.static(__dirname + "/../public/"));
+
+// Post level file.
+const LEVEL_ROUTE:String = "/level/:name";
+app.post(LEVEL_ROUTE, function(req, res) {
+    var levelName:String = req.params.name;
+    console.log(LEVEL_ROUTE.replace(":name", levelName));
+    var pathToFile:String = __dirname + "/../public/data/levels/" + levelName + ".json";
+    var levelJson = req.body;
+
+    // Save the file.
+    fs.writeFile(pathToFile, levelJson);
+
+    // Return download link.
+    res.download(pathToFile);
+    res.end();
+});
 
 // handle any 404 requests
 var routes = app._router.stack.filter(router => {return router.route}).map(router => {return router.route.stack.map(stack => {return `${stack.method.toUpperCase()} \t${router.route.path}`}).join("\n")}).join("\n");
@@ -15,4 +32,3 @@ app.listen(port, function(){
     console.log(`Listening to port:${port}`);
     console.log(`Routes:\n${routes}`);
 });
-
