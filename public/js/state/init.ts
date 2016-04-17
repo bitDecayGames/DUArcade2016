34cf8293bb7eabf4114218011ca5697f00b37d91
Jacob Caban-Tomski <jacob.caban-tomski@workiva.com>
2016-04-17 17:45:08 -0600

    Added in basic player walking animations.

--- public/js/state/init.ts
+++ public/js/state/init.ts
@@ -1,6 +1,7 @@
 class Init extends Phaser.State {
     preload(){
         this.load.json('gameInfo', 'conf/game.json');
+        this.load.spritesheet('playerSheet', 'img/player.png', 18, 45, 16);
         AssetsUtil.loadJson(this.load, Asset.IMAGES);
         AssetsUtil.loadJson(this.load, Asset.LEVELS);
     }
