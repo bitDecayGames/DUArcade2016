ed47eeddc38bb3d20bf18da662e68ba04fa187fc
Jacob Caban-Tomski <jacob.caban-tomski@workiva.com>
2016-04-16 19:17:57 -0600

    Added endpoint to grab all image assets.

--- server/app.ts
+++ server/app.ts
@@ -3,11 +3,13 @@
 var bodyParser = require('body-parser');
 var express = require("express");
 var fs = require('fs');
+var recursive = require('recursive-readdir');
 
 var app = express();
 var port = 3000;
 
 
+// Level endpoints
 const LEVEL_ROUTE:string = "/level/:name";
 app.post(LEVEL_ROUTE, bodyParser.json(), function(req, res) {
     var levelName:string = req.params.name;
@@ -20,6 +22,48 @@ app.post(LEVEL_ROUTE, bodyParser.json(), function(req, res) {
 
 
 app.get("/data/levels/:name", function(req, res){ res.download(__dirname + "/../public/data/levels/" + req.params.name) });
+
+// Image asset endpoints
+const IMAGE_ROUTE:string = "/images";
+const IMAGE_DIR:string  = __dirname + "/../public/img";
+/*
+ * Gets all images in the img directory.
+ *
+ * Format is:
+ * {
+ *      images: [
+ *          {
+ *              name: 'foo',
+ *              path: 'img/bar/foo.png'
+*           }
+ *      ]
+ * }
+ */
+app.get(IMAGE_ROUTE, bodyParser.json(), function(req, res) {
+    console.log(IMAGE_ROUTE);
+
+    recursive(IMAGE_DIR, function (err, files) {
+        var fileJson = {
+            images: []
+        };
+
+        files.forEach((filePath:string) => {
+            var strippedPath:string = filePath.substr(filePath.indexOf('img'));
+            var name:string = strippedPath.substr(strippedPath.lastIndexOf("/") + 1);
+            name = name.substr(0, name.lastIndexOf("."));
+
+            fileJson.images.push(
+                {
+                    name: name,
+                    path: strippedPath
+                }
+            );
+        });
+
+        res.send(JSON.stringify(fileJson, undefined, 4));
+        res.end();
+    });
+});
 
 
 app.use(express.static(__dirname + "/../public/"));
